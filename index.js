7c99a14e84ad9159d0a6cd34b66eb0893f4e3ac4
Riana Azad <Riana1996>
2022-11-10 09:33:46 +0600

    review loaded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -14,11 +14,52 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ifow7m.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('ghuraFira').collection('services');
+        const reviewCollection = client.db('ghuraFira').collection('reviews');
+
+        app.get('/services', async(req, res)=> {
+            const query ={}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // review api
+
+
+        app.post('/reviews', async(req, res)=>{
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+        app.get('/reviews', async(req, res)=> {
+            let query = {};
+            if(req.query.service_id){
+                query = {
+                    service_id: req.query.service_id
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+    }
+    finally{
+
+    }
+}
+
+run().catch(e => console.error(e));
 
 
 app.get('/', (req, res) =>{
